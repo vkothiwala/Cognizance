2db9699157c1c0c9bfec2af165265eea9cb2cb99
Varun Kothiwala <varunkothiwala@Varuns-MacBook-Pro.local>
2023-11-24 23:19:59 -0500

    Added Ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,9 @@
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint-idea")
 }
