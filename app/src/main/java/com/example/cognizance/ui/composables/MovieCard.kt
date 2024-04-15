3419f97d6adc21d94f3c1d285c3fe0d7e1ab7bdc
Varun Kothiwala <vkothiwala6@gmail.com>
2024-04-14 21:33:30 -0400

    Dark mode setup

--- app/src/main/java/com/example/cognizance/ui/composables/MovieCard.kt
+++ app/src/main/java/com/example/cognizance/ui/composables/MovieCard.kt
@@ -10,8 +10,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.example.ui.theme.WingTheme
